557d0ef4048353c06d63a0f1147b6164e5149609
evachen1010 <a0937510149@gmail.com>
2023-02-23 16:10:12 +0800

    註解

--- HW_2/5A9GG0006_HW2_1.js
+++ HW_2/5A9GG0006_HW2_1.js
@@ -1,13 +1,18 @@
+//範圍1~100
 for (var i = 1; i <= 100; i++) {
+    //是3和5的倍數先做輸出
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz") //輸出文字後換行
     }
+    //是3的倍數
     else if (i % 3 == 0) {
         console.log("Fizz")
     }
+    //是5的倍數
     else if (i % 5 == 0) {
         console.log("Buzz")
     }
+    //其餘的數字直接輸出
     else {
         console.log(i)
     }
